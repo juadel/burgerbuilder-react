74c78b7db434097eb16edb45218dd78b5f46edfc
Juan <juadel@hotmail.com>
2020-01-07 19:07:35 -0700

    State and setState

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons: [
+      {name: 'Juan', age: 28},
+      {name: 'Jaco', age: 10},
+      {name: 'Tomas', age: 8}
+    ]
+  }
+
+  switchNameHandler = () =>{
+    
+    // DONT DO this this.state.persons[0].name ="Carolina";
+    this.setState({
+     persons: [
+      {name: 'Carolina', age: 32},
+      {name: 'Jaco', age: 10},
+      {name: 'Tomas', age: 8}
+     ]
+  })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>HI, I'm a REACT App</h1>
+        <p> This is really working</p>
+        
+        <button onClick= {this.switchNameHandler}>Switch name</button>
+        <Person name={this.state.persons[0].name} age ={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age ={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age ={this.state.persons[2].age}/>
+        
       </div>
     );
+    //return React.createElement('div',{className:"App"},React.createElement('h1',null,'Does this works now'))
   }
 }
 
